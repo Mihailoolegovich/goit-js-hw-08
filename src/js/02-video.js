ff5011d57cf01919628c3b4be0ce36472c14676b
Mihailoolegovich <mihailoolegovich@gmail.com>
2021-11-27 00:47:49 +0200

    ver. 1.03

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo.Player(iframe);
+const TIME_UP_DATE = 'videoplayer-current-time';
+//  videoplayer-current-time
 
+// player.on('play', function (data) {
+//   console.log('played the video!');
+//   console.log('player', data.seconds);
+// });
+// getTimeVideo();
+player.on('timeupdate', saveTimeVideo);
+
+// player.setCurrentTime(132.11);
+
+//
+
+// localStorage.getItem(TIME_UP_DATE);
+
+// function cashTimeVideo() {}
+
+function saveTimeVideo(params) {
+  localStorage.setItem(TIME_UP_DATE, params.seconds);
+//   getTimeVideo()
+}
+
+// function getTimeVideo() {
+//   console.log('getTimeVideo', localStorage.getItem(TIME_UP_DATE));
+//   localStorage.getItem(TIME_UP_DATE);
+// }
+player.setCurrentTime(localStorage.getItem(TIME_UP_DATE));
